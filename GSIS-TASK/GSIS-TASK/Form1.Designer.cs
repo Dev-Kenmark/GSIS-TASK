9d033b906f28c2388512020e5d2f6fc56b64ae52
KennyTzy <91226502+KennyTzy@users.noreply.github.com>
2022-02-02 19:22:14 +0800

    Github Pa more

--- GSIS-TASK/GSIS-TASK/Form1.Designer.cs
+++ GSIS-TASK/GSIS-TASK/Form1.Designer.cs
@@ -40,6 +40,7 @@ namespace GSIS_TASK
             this.button4 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
@@ -97,7 +98,7 @@ namespace GSIS_TASK
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(227, 500);
+            this.label1.Location = new System.Drawing.Point(212, 511);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(566, 89);
             this.label1.TabIndex = 2;
@@ -150,11 +151,22 @@ namespace GSIS_TASK
             this.label3.TabIndex = 7;
             this.label3.Text = "KENMARK SAKALAM";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(212, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(566, 89);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "GITHUB PA MORE";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1000, 618);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button4);
@@ -187,6 +199,7 @@ namespace GSIS_TASK
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
