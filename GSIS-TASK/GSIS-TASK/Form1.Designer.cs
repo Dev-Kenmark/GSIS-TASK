a6d9c83b093e1745aa3cee84c3b61d5f31217970
KennyTzy <91226502+KennyTzy@users.noreply.github.com>
2022-02-02 19:23:02 +0800

    2

--- GSIS-TASK/GSIS-TASK/Form1.Designer.cs
+++ GSIS-TASK/GSIS-TASK/Form1.Designer.cs
@@ -52,7 +52,7 @@ namespace GSIS_TASK
             this.tabControl1.Location = new System.Drawing.Point(246, 124);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(509, 384);
+            this.tabControl1.Size = new System.Drawing.Size(509, 363);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -79,10 +79,11 @@ namespace GSIS_TASK
             this.tabPage2.Location = new System.Drawing.Point(4, 24);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(501, 356);
+            this.tabPage2.Size = new System.Drawing.Size(501, 335);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
+    
             // 
             // button1
             // 
@@ -140,7 +141,30 @@ namespace GSIS_TASK
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 89);
             this.label2.TabIndex = 6;
+<<<<<<< Updated upstream
             this.label2.Click += new System.EventHandler(this.label2_Click);
+=======
+            this.label2.Text = "ANG SAYA SA ALLCARD TARA NA";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(501, 335);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+>>>>>>> Stashed changes
             // 
             // label3
             // 
---
+++ GSIS-TASK/GSIS-TASK/Form2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GSIS_TASK
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
